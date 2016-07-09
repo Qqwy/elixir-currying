bc9189ae53bd3727e57412a1adf2140fba587214
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-07-09 22:24:52 +0200

    bump version number.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Currying.Mixfile do
 
   def project do
     [app: :currying,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
